0fb783b3857748d742ec8e3ec15af7818d170300
johnalexsagwe <johnsagwe@gmail>
2024-02-23 00:24:14 -0800

    Pycode style issues fixed

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,7 @@
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-#from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
